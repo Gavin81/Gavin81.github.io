6b7068c83ca209efa3677300ddae97e4af4ba7aa
Gavin81 <g_eyquem@hotmail.com>
2025-09-03 18:53:23 +0100

    html css js

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const header = document.getElementByTagName('header');
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 
+
 // hamMenu.addEventListener('click', () => {
 //     hamMenu.classList.toggle('active');
 //     offScreenMenu.classList.toggle('active');
 // })
 
 header.classList.toggle('sticky', isPastHeader);
+
+
