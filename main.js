224c586abb9c8422909b484cc7d240d8ac5c0330
Gavin81 <g_eyquem@hotmail.com>
2025-09-11 14:51:25 +0100

    html, css and js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const header = document.getElementByTagName('header');
 const hamMenu = document.querySelector('.ham-menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
+const accordionButton = document.querySelector('accordion h2');
+// const accordion = document.querySelectorAll('.accordion');
 
 
 // hamMenu.addEventListener('click', () => {
@@ -10,4 +12,39 @@ const offScreenMenu = document.querySelector('.off-screen-menu');
 
 header.classList.toggle('sticky', isPastHeader);
 
+
+accordionButton.addEventListener('click', e => {
+    p.classList.toggle('active');
+    if(accordionButton.textContent === '+'){
+        accordionButton.textContent = '-';
+    } else {
+        accordionButton.textContent = '+';
+    }
+})
+
+
+// accordion.forEach(accordion => {
+//     accordion.addEventListener('click', e => {
+//         let accordionBtn = accordion.querySelector('button');
+//         accordion.classList.toggle('active');
+//         if(accordionBtn.textContent === '+'){
+//             accordionBtn.textContent = '-';
+//         } else {
+//             accordionBtn.textContent = '+';
+//         }
+//     })
+// })
+
+// const accordions = document.querySelectorAll('.accordion');
+// accordions.forEach(accordion => {
+//   accordion.addEventListener('click', e => {
+//     let accordionBtn = accordion.querySelector('button');
+//     accordion.classList.toggle('active');
+//     if (accordionBtn.textContent === '+') {
+//       accordionBtn.textContent = '-';
+//     } else {
+//       accordionBtn.textContent = '+';
+//     }
+//   })
+// })
 
